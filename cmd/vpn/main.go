61cb7756c4a58b50fb66466b8e6c35b3e9d95fb0
Corey Prophitt <git@prophitt.me>
2023-02-02 05:43:20 -0800

    Added vpn command line  tool

--- cmd/vpn/main.go
+++ cmd/vpn/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/prophittcorey/vpn"
+)
 
 func main() {
-	fmt.Println("TODO: Need to work on this.")
+	var vpns bool
+	var ip string
+
+	flag.BoolVar(&vpns, "subnets", false, "if specified, all VPN subnets will be dumped in one list")
+	flag.StringVar(&ip, "check", "", "an ip address to analyze (returns information about the address)")
+
+	flag.Parse()
+
+	if vpns {
+		for _, subnet := range vpn.Subnets() {
+			fmt.Printf("%s\n", subnet.String())
+		}
+
+		return
+	}
+
+	if len(ip) > 0 {
+		name, err := vpn.Check(ip)
+
+		if err == nil {
+			fmt.Printf("Looks like a '%s' address.\n", name)
+		} else {
+			fmt.Printf("Does not look like a vpn address.\n")
+		}
+
+		return
+	}
+
+	flag.Usage()
 }
