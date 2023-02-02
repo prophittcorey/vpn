8c78729e4cf8b4486a471c12ab3789e16b3165c9
Corey Prophitt <git@prophitt.me>
2023-02-02 05:28:28 -0800

    Added Check method signature

--- vpn.go
+++ vpn.go
@@ -1,1 +1,11 @@
 package vpn
+
+import "fmt"
+
+const (
+	ErrNotFound = fmt.Errorf(`error: does not appear to be through a known vpn`)
+)
+
+func Check(addr string) (string, error) {
+	return "", ErrNotFound
+}
